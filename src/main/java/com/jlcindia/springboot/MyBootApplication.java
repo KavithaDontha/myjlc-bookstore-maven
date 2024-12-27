9be3a7d3e2cf729d0e455531020d10838667d5ff
KavithaDontha <kavithaprasadv@gmail.com>
2024-12-27 12:45:06 +0800

    Update -7 by Kavitha

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -8,11 +8,12 @@ public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
 System.out.println("Update -1 Srinivas");
-  System.out.println("Update -2 by Srinivas");
-   System.out.println("Update -3 by Kavitha");
-   System.out.println("Update -4 by Kavitha");
-   System.out.println("Update -5 by Kavitha");
-     System.out.println("Update -6 by Kavitha");
+System.out.println("Update -2 by Srinivas");
+System.out.println("Update -3 by Kavitha");
+System.out.println("Update -4 by Kavitha");
+System.out.println("Update -5 by Kavitha");
+System.out.println("Update -6 by Kavitha");
+System.out.println("Update -7 by Kavitha");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
